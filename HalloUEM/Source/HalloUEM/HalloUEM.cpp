c6e05d397ce470f2da6d45fa086121da936986c3
Mario <mario.rodavila@gmail.com>
2018-10-27 01:24:08 +0200

    proyecto creado

---
+++ HalloUEM/Source/HalloUEM/HalloUEM.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HalloUEM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HalloUEM, "HalloUEM" );
