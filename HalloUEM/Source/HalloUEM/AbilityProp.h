9b798be7ddc214ad729f42b9fdc7b89cc97024ec
Carlos <cdiezm14@gmail.com>
2018-10-27 03:25:11 +0200

    Creado Player, creado AbilityProp

---
+++ HalloUEM/Source/HalloUEM/AbilityProp.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperSpriteActor.h"
+#include "AbilityProp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HALLOUEM_API AAbilityProp : public APaperSpriteActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
